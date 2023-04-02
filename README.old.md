59bb1d6c87176f5c9a03c531eed6d47c13072486
Owais Khan <45107209+kowais915@users.noreply.github.com>
2023-04-02 16:00:15 +0500

    :tada: Setup the boilerplate

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# gret-giki.github.io
+The GIKI Rocket Engineering Team's official website. 
---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">The GIKI Rocket Engineering Team</h1>
+    </div>
+  );
+}
+
+export default App;
